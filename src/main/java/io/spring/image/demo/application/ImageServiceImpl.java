308c39f607a0110f7496a46a55cbeb7ce07cb5de
yasminaraujos <yasmin.araujo@estudante.ifmt.edu.br>
2026-04-22 10:24:29 -0400

    Camada de Service

---
+++ src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
@@ -0,0 +1,22 @@
+package io.spring.image.demo.application;
+
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.domain.service.ImageService;
+import io.spring.image.demo.infra.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image) {
+        return null;
+    }
+}
---
+++ src/main/java/io/spring/image/demo/domain/service/ImageService.java
@@ -0,0 +1,8 @@
+package io.spring.image.demo.domain.service;
+
+import io.spring.image.demo.domain.entity.Image;
+
+public interface ImageService {
+
+    Image save (Image image);
+}
